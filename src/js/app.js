d79929090ef95161a8f199a53301dee2a4d6de29
joyce-song <18598246268@163.com>
2017-02-16 21:12:35 +0800

    update

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,5 @@ import'./components/nav';
 import'./components/goTop';
 import'./components/pageAnimation';
 import'./components/operateDom';
+console.log('本站由Joyce Song提供技术支持，技术交流请进入我的博客 http://www.joycesong.com 获取联系方式，感谢您的支持！');
 
